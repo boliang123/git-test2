6f5d038076b075fcfbfb1074878230be2e106aee
Boliang <2021155841@qq.com>
2022-01-07 15:44:29 +0800

    push SSH1  cimmit

--- src/main/java/atguigu/git/test/Gittest.java
+++ src/main/java/atguigu/git/test/Gittest.java
@@ -9,5 +9,6 @@ public class Gittest {
         System.out.println("mater test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("push SSH test");
     }
 }
