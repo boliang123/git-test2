f50d361c299f0bed4a03c04c32f863dff7b0a958
Boliang <2021155841@qq.com>
2022-01-07 15:41:56 +0800

    push 1  cimmit

--- src/main/java/atguigu/git/test/Gittest.java
+++ src/main/java/atguigu/git/test/Gittest.java
@@ -8,5 +8,6 @@ public class Gittest {
         System.out.println("Hello Git4");
         System.out.println("mater test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
