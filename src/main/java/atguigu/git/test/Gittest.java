84c650a352f6f69eb0e711e2086d49550b15718f
Boliang <2021155841@qq.com>
2022-01-07 15:06:17 +0800

    2 cimmit

--- src/main/java/atguigu/git/test/Gittest.java
+++ src/main/java/atguigu/git/test/Gittest.java
@@ -3,5 +3,6 @@ package atguigu.git.test;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
     }
 }
